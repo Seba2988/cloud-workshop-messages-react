67f86e97455f278463506d083db61cd3a3cfce33
Sebastian Alvarez <seba.29.al@gmail.com>
2022-10-25 13:56:53 +0300

    url changed

--- src/server/messages.ts
+++ src/server/messages.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const URL = "http://localhost:8080/";
-
+// const URL = "http://localhost:8080/";
+const URL = "http://ec2-34-243-34-230.eu-west-1.compute.amazonaws.com/";
 const getMessages = async () => {
 	try {
 		const response = await axios.get(URL + "messages");
